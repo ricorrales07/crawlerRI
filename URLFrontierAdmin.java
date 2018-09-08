9c0cdfddd4f8ff6d7b933b91c74360d23b04794f
Ricardo Corrales Barquero <ricorrales07@gmail.com>
2018-09-07 18:51:20 -0600

    Tengo un poco enredada la clase URLFrontierAdmin. Creo que necesitamos más clases.

--- URLFrontierAdmin.java
+++ URLFrontierAdmin.java
@@ -1,18 +1,29 @@
-import java.util.Queue;
+import java.net.InetAddress;
+import java.time.Instant;
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by Ricardo on 5/9/2018.
  */
 public class URLFrontierAdmin {
 
-    private Queue[] frontQueues, backQueues;
-    public int F, B;
+    private ArrayDeque<WebPage>[] frontQueues, backQueues;
+    private int F, B;
+    private ConcurrentHashMap<InetAddress, ArrayDeque<WebPage>> hostTable;
+    //private PriorityQueue<InetAddress> priorityHeap;
 
     public URLFrontierAdmin(int F, int B) {
         this.F = F;
         this.B = B;
-        frontQueues = new Queue[F];
-        backQueues = new Queue[B];
+        frontQueues = new ArrayDeque[F];
+        for (int i = 0; i < F; i++)
+            frontQueues[i] = new ArrayDeque<WebPage>();
+        backQueues = new ArrayDeque[B];
+        for (int i = 0; i < B; i++)
+            backQueues[i] = new ArrayDeque<WebPage>();
+        hostTable = new ConcurrentHashMap<InetAddress, ArrayDeque<WebPage>>();
     }
 
     public String getNextURL() {
